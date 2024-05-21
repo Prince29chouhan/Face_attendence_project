c5535045d7145fbed6fa742c4b9028e92093b0fa
Prince Chouhan <princechouhan98765@gmail.com>
2024-05-21 08:42:29 +0530

    updated

--- faceRec.py
+++ faceRec.py
@@ -7,10 +7,14 @@ reference_faces_dir = 'reference_faces'
 reference_faces = []
 reference_face_names = []
 
-for filename in os.listdir(reference_faces_dir):
-    img = face_recognition.load_image_file(os.path.join(reference_faces_dir, filename))
-    reference_faces.append(face_recognition.face_encodings(img)[0])
-    reference_face_names.append(os.path.splitext(filename)[0])
+for foldername in os.listdir(reference_faces_dir):
+    folder_path = os.path.join(reference_faces_dir, foldername)
+    if not os.path.isdir(folder_path):
+        continue
+    for filename in os.listdir(folder_path):
+        img = face_recognition.load_image_file(os.path.join(folder_path, filename))
+        reference_faces.append(face_recognition.face_encodings(img)[0])
+        reference_face_names.append(foldername)
 
 # Load the test face images
 test_faces_dir = 'test_faces'
